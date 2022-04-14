a114cc5daadba318ca02defc9b325f953c43220f
Olivier B. OURA <baudoliver7@gmail.com>
2022-04-14 04:16:23 +0000

    feat: create budget expenditure details
    Close: #40

---
+++ src/main/java/io/surati/gap/maccount/module/web/pages/TkBudgetExpenditureDetailsPreview.java
@@ -0,0 +1,41 @@
+package io.surati.gap.maccount.module.web.pages;
+
+import io.surati.gap.web.base.RsPage;
+import io.surati.gap.web.base.xe.XeRootPage;
+import javax.sql.DataSource;
+import org.cactoos.iterable.Sticky;
+import org.takes.rq.RqHref;
+import org.takes.rs.xe.XeAppend;
+import org.takes.rs.xe.XeChain;
+import org.takes.rs.xe.XeSource;
+import org.takes.tk.TkWrap;
+
+public final class TkBudgetExpenditureDetailsPreview extends TkWrap {
+
+    public TkBudgetExpenditureDetailsPreview(final DataSource src) {
+        super(
+            req -> {
+                final XeSource srce = new XeChain(
+                    new XeAppend(
+                        "report_url",
+                        String.format(
+                            "/maccount/report/budget-expenditure-details/print?year=%s&treasury=%s",
+                            new RqHref.Smart(req).single("year"),
+                            new RqHref.Smart(req).single("treasury")
+                        )
+                    )
+                );
+                return new RsPage(
+                    "/io/surati/gap/maccount/module/xsl/report/report_preview.xsl",
+                    req,
+                    src,
+                    ()-> new Sticky<>(
+                        new XeRootPage(req),
+                        srce
+                    )
+                );
+
+            }
+        );
+    }
+}

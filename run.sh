01b1ca62f3cfca5ee91778ca7cc4f7968c642e3f
Olivier B. OURA <baudoliver7@gmail.com>
2022-04-14 10:26:54 +0000

    fix: db connection driver

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 java --add-opens java.base/java.lang=ALL-UNNAMED \
   -cp /usr/lib/gap/gap.jar:/usr/lib/gap/lib/* io.surati.gap.maccount.module.web.server.Main \
-  --port=8080 --driver=org.postgresql.Driver \
+  --port=8080 --db-driver=org.postgresql.Driver \
   --db-url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME} \
   --db-user=${DB_USER} --db-password=${DB_PASSWORD} \
   --threads=${NB_THREADS}
